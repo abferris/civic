a53fd857329a1f4a9b0f42b3c7c414456a406b2e
Janice Bradford <janice.bradford@outlook.com>
2019-04-23 12:53:46 -0700

    WIP add knobs to Button story.

--- packages/component-library/stories/DropdownMenu.story.js
+++ packages/component-library/stories/DropdownMenu.story.js
@@ -1,38 +1,69 @@
 import React from "react";
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from "@storybook/react";
-import { withInfo } from "@storybook/addon-info";
 import { checkA11y } from "@storybook/addon-a11y";
+import { withKnobs, text, color } from "@storybook/addon-knobs";
 import { Dropdown } from "../src";
 import { storybookStyles } from "./storyStyles.js";
 
-const displayName = Dropdown.displayName || "Dropdown";
-const title = "Simple usage";
 const description = `
   This is some basic usage with the button with providing a label to show the text.
   Clicking should trigger an action.`;
 
-const demoCode = () => (
-  <Dropdown
-    dispatch={dispatch => action => dispatch(action)}
-    reduxAction={payload => console.log({ type: "ACTION", payload })}
-    options={[
-      { value: "0", label: "David" },
-      { value: "1", label: "Daniel" },
-      { value: "2", label: "Last" },
-      { value: "3", label: "Name" }
-    ]}
-  />
-);
-
 // const propDocs = { inline: true, propTables: [Dropdown] };
 
 export default () =>
-  storiesOf("Component Lib|Basic UI Components/Dropdown List", module)
+  storiesOf("Component Lib|Basic Inputs/Dropdown List", module)
     .addDecorator(checkA11y)
     .addDecorator(story => (
       <div style={storybookStyles.storyGrid}>
         <div style={storybookStyles.storyGridItem}>{story()}</div>
       </div>
     ))
-    .add(title, demoCode);
+    .add("Simple usage", () => {
+      const options = [
+        { value: "0", label: "Murphy" },
+        { value: "1", label: "Carter" },
+        { value: "2", label: "Bebe" },
+        { value: "3", label: "Cissi" }
+      ];
+      return (
+        <Dropdown
+          dispatch={dispatch => action => dispatch(action)}
+          reduxAction={payload => console.log({ type: "ACTION", payload })}
+          options={options}
+        />
+      );
+    })
+    .add("Basic usage", () => {
+      const options = [
+        { value: "0", label: "Murphy" },
+        { value: "1", label: "Carter" },
+        { value: "2", label: "Bebe" },
+        { value: "3", label: "Cissi" }
+      ];
+      return (
+        <Dropdown
+          dispatch={dispatch => action => dispatch(action)}
+          reduxAction={payload => console.log({ type: "ACTION", payload })}
+          options={options}
+        />
+      );
+    })
+    .add("Multi-select", () => {
+      const options = [
+        { value: "0", label: "Murphy" },
+        { value: "1", label: "Carter" },
+        { value: "2", label: "Bebe" },
+        { value: "3", label: "Cissi" }
+      ];
+      const multi = true;
+      return (
+        <Dropdown
+          dispatch={dispatch => action => dispatch(action)}
+          reduxAction={payload => console.log({ type: "ACTION", payload })}
+          options={options}
+          multi={multi}
+        />
+      );
+    });
